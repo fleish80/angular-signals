0b1b3329255809cc8dc7bb679cb578043f5cc9b5
fleish80 <Fleish.D@gmail.com>
2024-01-28 18:31:57 +0200

    signals

---
+++ libs/signals-store/src/index.ts
@@ -0,0 +1,13 @@
+export { getState } from './lib/get-state';
+export { PartialStateUpdater, patchState } from './lib/patch-state';
+export { signalState } from './lib/signal-state';
+export { signalStore } from './lib/signal-store';
+export { signalStoreFeature, type } from './lib/signal-store-feature';
+export { SignalStoreFeature } from './lib/signal-store-models';
+
+export { withComputed } from './lib/with-computed';
+export { withHooks } from './lib/with-hooks';
+export { withMethods } from './lib/with-methods';
+export { withState } from './lib/with-state';
+
+export { rxMethod } from './lib/rx-method';
