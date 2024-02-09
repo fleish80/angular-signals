32266af829455c75da45d336252bbe14a2afc82e
fleish80 <Fleish.D@gmail.com>
2024-02-09 15:42:31 +0200

    signals

--- apps/main/src/app/state-management/todos-entities/todo.component.ts
+++ apps/main/src/app/state-management/todos-entities/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { JsonPipe } from '@angular/common';
 import { TodoTableComponent } from './todo-table.component';
 import { TodoStore } from './todo.store';
@@ -10,11 +10,12 @@ import { TodoStore } from './todo.store';
     JsonPipe,
     TodoTableComponent
   ],
-  template: `<df-todo-table/>`,
+  template: `<pre>{{ todoEntities()[1] | json }}</pre>`,
   providers: [TodoStore]
 })
 export default class TodoComponent {
-
+  #todoStore = inject(TodoStore);
+  todoEntities = this.#todoStore.todoEntityMap;
 
 }
 
